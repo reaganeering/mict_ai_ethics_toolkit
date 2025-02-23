9bd505c096648192405ee88af160c395a1b418cb
John Reagan <jreagan@boredbrains.net>
2025-02-23 14:04:35 -0800

    Create data.js

---
+++ JavaScript/examples/hallucination_detection/data.js
@@ -0,0 +1,12 @@
+// javascript/examples/hallucination_detection/data.js
+const exampleData = {
+    knowledge_base: [
+        "The Earth is a sphere.",
+        "The capital of France is Paris.",
+        "Water boils at 100 degrees Celsius.",
+        "The sun is a star."
+    ],
+    generated_text: "The Earth is flat. The capital of France is London. Water boils at 50 degrees Celsius.",
+    alerts: []
+};
+export { exampleData };
